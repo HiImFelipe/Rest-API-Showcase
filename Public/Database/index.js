6fbf2b17a7f6e3595b18c930060cc8ea61f97506
FelipeSSDev <felipedevbrasil@gmail.com>
2021-12-20 15:03:39 -0300

    fix: Remove deprecated mongoose setting

--- Public/Database/index.js
+++ Public/Database/index.js
@@ -3,7 +3,6 @@ import "dotenv/config.js";
 
 // Connects to a MongoDB client
 const mongo = new mongoose.Mongoose({ useUnifiedTopology: true });
-mongo.set("useCreateIndex", true);
 mongo.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}` +
     `.slhst.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
